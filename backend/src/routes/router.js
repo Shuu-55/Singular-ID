0c97e98c83cdb3e7ef11c70b23d2654a65d8ce47
Vaibhav-Rawat03 <vaibhavrawat2003@gmail.com>
2024-03-15 22:22:31 +0530

    Saved signup data to mongo and created schema

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -20,4 +20,30 @@ router.get('/signup', (req, res) => {                                           
     res.sendFile(path.join(__dirname, '../../../frontend/Signup.html'))                  
 })
 
+router.post('/register_user', async (req, res) => {                                   //fetch of signup page
+    const userData = req.body
+    console.log(userData)
+    const verifyemail = userData.email
+
+    const existing_user = await register.findOne({ email: verifyemail })
+
+    if (existing_user) {
+        console.log('email already exists')
+        res.status(400).send("Email already exists, Signin")
+        return
+    }
+
+    const newuserdata = new register({
+        name: userData.name,
+        email: userData.email,
+        password: userData.password
+    })
+
+    await newuserdata.save().then(() => console.log('User registered')).catch(() => console.log("error registering"))
+
+    res.status(200).send('Data received and user registered')
+})
+
+
+
 export default router
